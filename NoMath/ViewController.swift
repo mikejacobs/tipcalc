91852da501c45bf2d08908e0c8f5fa9104c9adcd
Mike Jacobs <jacobs@uber.com>
2015-05-02 17:53:17 -0700

    completed tip calculator

--- NoMath/ViewController.swift
+++ NoMath/ViewController.swift
@@ -10,11 +10,61 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var tipView: UIView!
+    var firstRun = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        billField.text = "$"
+        billField.becomeFirstResponder()
+        tipLabel.text = "$0.00"
+        totalLabel.text = "$0.00"
     }
+    
+    @IBAction func onBillChanged(sender: AnyObject) {
+        if(firstRun){
+            println("first")
+            firstRun = false
+            UIView.animateWithDuration(0.3, animations: {
+                
+                self.tipView.frame = CGRect(x: self.tipView.frame.origin.x, y: self.tipView.frame.origin.y - 20, width: self.tipView.frame.width, height: self.tipView.frame.height)
+                self.tipView.alpha = 1
+                
+                }, completion: nil)
+        }
+        
+        
+        var tipPercentages = [0.15, 0.2, 0.25]
+        var tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
 
+        var billString = ""
+        var billAmount: Double = 0.0
+        var tip: Double = 0.0
+        var total: Double = 0.0
+        
+//      if text isn't just a dollar sign
+        if(countElements(billField.text) > 1){
+            
+            billString = billField.text.substringFromIndex(advance(billField.text.startIndex,1))
+            var billAmount : Double = NSString(string: billString).doubleValue
+            tip = billAmount * tipPercentage
+            total = billAmount + tip
+            
+        }
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+        billField.text = "$\(billString)"
+    }
+    
+    @IBAction func onTapView(sender: AnyObject) {
+        view.endEditing(true)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
